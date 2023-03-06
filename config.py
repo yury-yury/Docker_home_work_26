8a0e70440c73f9604ed783114f28dc65f5e2c42b
yury-yury <yury_yury@mail.ru>
2023-03-07 01:45:34 +0300

    Joy

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DB_USER: str = os.getenv('DB_USER')
     DB_PASSWORD: str = os.getenv('DB_PASSWORD')
     DB_HOST: str = os.getenv('DB_HOST')
-    DB_PORT: int = 5432
+    DB_PORT: int = os.getenv('DB_PORT')
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
